eabc74d3409010dc12a9e9df3af56a1ed7ff015a
Thomas Chafiol <thomaschaf@gmail.com>
2014-11-08 23:38:26 +0100

    add config file and translator

---
+++ web/app/config/config.php
@@ -0,0 +1,23 @@
+error_404 = "error"
+
+home = "/"
+
+default_URL = "home"
+default_language = "en"
+
+languages[] = "en"
+languages[] = "us"
+languages[] = "fr"
+
+routes[] = "home"
+routes[] = "about"
+routes[] = "raytracer"
+routes[] = "shell42"
+routes[] = "colorPicker"
+
+
+link["facebook"] = "https://www.facebook.com/thomas.chafiol.1"
+link["twitter"] = "https://twitter.com/ThomasChaf"
+link["linkedin"] = "https://www.linkedin.com/profile/view?id=313822159"
+link["github"] = "https://github.com/ThomasChaf"
+link["resume"] = "https://docs.google.com/document/d/1Qsd-qqYeOvV9_5eAwHxMfJCa2-Cblw9MQiCl2Psxt1E"
